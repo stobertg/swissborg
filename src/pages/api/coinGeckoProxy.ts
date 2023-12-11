876f1dd172b98e732a59a0e5bdd2fafbdbf8df44
Tyler Stober <stobertg@gmail.com>
2023-12-11 09:06:47 +0000

    fixing api call and finished the market header

---
+++ src/pages/api/coinGeckoProxy.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosRequestConfig } from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const API_KEY = process.env.COINGECKO_API_KEY
+const BASE_URL = 'https://api.coingecko.com/api/v3'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const { days } = req.query
+
+    const config: AxiosRequestConfig = {
+      headers: API_KEY ? { 'Authorization': `Bearer ${API_KEY}` } : {}
+    }
+
+    const detailResponse = await axios.get(`${BASE_URL}/coins/swissborg`, config)
+    const circulatingSupply = detailResponse.data.market_data.circulating_supply
+    const maxSupply = detailResponse.data.market_data.max_supply
+
+    const fetchMarketData = async (days: string) => {
+      const marketChartUrl = `${BASE_URL}/coins/swissborg/market_chart?vs_currency=usd&days=${days}`
+      const response = await axios.get(marketChartUrl, config)
+      return response.data.prices
+    };
+
+    const prices24h = await fetchMarketData('1')
+    const prices1m = await fetchMarketData('30')
+    const prices1y = await fetchMarketData('365')
+    const pricesAll = await fetchMarketData('max')
+
+    const aggregatedData = {
+      metadata: detailResponse.data,
+      circulatingSupply,
+      maxSupply,
+      prices24h,
+      prices1m,
+      prices1y,
+      pricesAll
+    };
+
+    res.status(200).json(aggregatedData)
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching data from CoinGecko' })
+  }
+}
--- src/pages/api/supply.ts
+++ src/pages/api/supply.ts
@@ -1,49 +1,15 @@
-import axios from 'axios';
-
-const BASE_URL = 'https://api.coingecko.com/api/v3';
-const API_KEY = process.env.NEXT_PUBLIC_COINGECKO_API;
+import axios from 'axios'
 
 export const getBorgMarketSupply = async () => {
   try {
-    // Fetch details including circulating and max supply
-    const detailResponse = await axios.get(`${BASE_URL}/coins/swissborg`, {
-      headers: {
-        'Authorization': `Bearer ${ API_KEY }`
-      }
-    });
 
-    const circulatingSupply = detailResponse.data.market_data.circulating_supply;
-    const maxSupply = detailResponse.data.market_data.max_supply;
+    const response = await axios.get('/api/coinGeckoProxy', {
+      params: { days: '365' }
+    })
 
-    // Function to fetch market data for different time frames
-    const fetchMarketData = async (days:any) => {
-      const marketChartUrl = `${ BASE_URL }/coins/swissborg/market_chart?vs_currency=usd&days=${ days }`;
-      const response = await axios.get(marketChartUrl, {
-        headers: {
-          'Authorization': `Bearer ${API_KEY}`
-        }
-      });
-      return response.data.prices; // Array of [timestamp, price]
-    };
-
-    // Fetch market data for different time frames
-    const prices24h = await fetchMarketData('1');
-    const prices1m = await fetchMarketData('30');
-    const prices1y = await fetchMarketData('365');
-    const pricesAll = await fetchMarketData('max'); 
-
-    return {
-      metadata: detailResponse.data,
-      circulatingSupply,
-      maxSupply,
-      prices24h,
-      prices1m,
-      prices1y,
-      pricesAll
-    };
+    return response.data
   } catch (error) {
-    console.error('Error fetching Swissborg details and market data:', error);
-    throw error;
+    console.error('Error fetching data via proxy:', error)
+    throw error
   }
 };
-
